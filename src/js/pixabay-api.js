7d1b8dd0bbc1360d186b1e5cc8664ea58661ccc7
andrei_gubsky <anrei.gubsky@gmail.com>
2025-10-09 08:37:19 +0300

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,9 @@
+// getImagesByQuery(query). 
+// Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком), 
+// здійснювати HTTP-запит 
+// і повертати значення властивості data з отриманої відповіді.
+
+let query = '';
+function getImagesByQuery(query){
+    return Response.data;
+}
